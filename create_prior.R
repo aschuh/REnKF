3e3108d75ebc63203793486e1b980a04f18a4e2e
Andrew E. Schuh <aschuh@discover16.prv.cube>
2013-11-13 11:56:16 -0500

    adding scaling term arg

--- create_prior.R
+++ create_prior.R
@@ -3,7 +3,7 @@
 #-- just arithmetic mean, NOT variance weighted mean as it should be, let's fix that later
 #-- ...
 
-create_prior = function(ifiles,pr_ind,estimate_land_ocean_bias,ensembles)
+create_prior = function(ifiles,pr_ind,estimate_land_ocean_bias,ensembles,grand_prior_scaling=1)
 {
 	for(k in 1:length(ifiles))
         {
@@ -25,6 +25,12 @@ create_prior = function(ifiles,pr_ind,estimate_land_ocean_bias,ensembles)
                        BETARESP[,,j] = ncvar_get(ifiles.fil,"BETARESP",start=c(1,1,samp[j],1),count=c(-1,-1,1,1))#/length(ifiles)
                        BETAOCEAN[,,j] = ncvar_get(ifiles.fil,"BETAOCEAN",start=c(1,1,samp[j],1),count=c(-1,-1,1,1))#/length(ifiles)
                        }
+
+                 #-- Scale down initial deviations
+                 BETAGPP = BETAGPP*grand_prior_scaling
+                 BETARESP = BETARESP*grand_prior_scaling
+                 BETAOCEAN = BETAOCEAN*grand_prior_scaling
+
                  #-- Important, we need to convert deviations to variations before we weight, and add
                  BETAGPP[,,2:ensembles]   =  aperm(aaply(BETAGPP[,,2:ensembles],c(3),.fun=function(x){x-BETAGPP[,,1]}),c(2,3,1))
                  BETARESP[,,2:ensembles]  =  aperm(aaply(BETARESP[,,2:ensembles],c(3),.fun=function(x){x-BETARESP[,,1]}),c(2,3,1))
